8368927798d3dea46aa1edd5022ec62276942bba
natagetup <n.kulikouskaya@gmail.com>
2023-12-12 19:38:29 +0100

    feat: implement delete-digit task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const obj = {};
+
+ domains.forEach(domain => {
+  const tempArr = domain.split('.'); //split domain into array with separate words
+  let tempStr = '';
+
+  for (let k = tempArr.length - 1; k >= 0; k--) {
+    //make a new key for object
+    tempStr += `.${tempArr[k]}`;
+
+    //check this key presence and add value
+    if (obj[`${tempStr}`]) {
+      obj[`${tempStr}`] += 1;
+    } else {
+      obj[`${tempStr}`] = 1;
+    }
+  }
+}) 
+
+  return obj;
 }
 
 module.exports = {
