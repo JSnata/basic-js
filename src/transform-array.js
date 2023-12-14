2d3e84038a18ea25ad31240097d57ecced36b32e
natagetup <n.kulikouskaya@gmail.com>
2023-12-14 15:12:04 +0100

    feat: add transform-array task implementation

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,63 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  let controlCounts = [
+    '--discard-next',
+    '--discard-prev',
+    '--double-next',
+    '--double-prev'
+  ]
+
+  if (Array.isArray(arr)) {
+    let newArr = [];
+
+    for(let i = 0; i < arr.length; i++) {
+      if (arr[i] === controlCounts[0]) {
+        if (i !== arr.length - 1) {
+          i++;
+        } else {
+          continue;
+        }
+      } else if (arr[i] === controlCounts[1]) {
+        if (arr[i - 2] === controlCounts[0]){
+          continue;
+        }
+        else if(i !== 0) {
+          newArr.pop();
+        } else {
+          continue;
+        }
+      } else if (arr[i] === controlCounts[2]) {
+        if (i !== arr.length - 1) {
+          newArr.push(arr[i + 1]);
+          newArr.push(arr[i + 1]);
+          i++;
+        } else {
+          continue;
+        }
+
+      } else if (arr[i] === controlCounts[3]){
+
+        if (arr[i - 2] === controlCounts[0]){
+          continue;
+        }
+           else if(i !== 0) {
+            newArr.push(newArr[newArr.length - 1]);
+          }
+            else {
+            continue;
+          }
+      }
+      else {
+        newArr.push(arr[i]);
+      }
+    }
+
+    return newArr;
+  } 
+
+  throw new Error(`'arr' parameter must be an instance of the Array!`);
 }
 
 module.exports = {
