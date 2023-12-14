65ba16b1669898265abf954fe24e7ea6065090ce
natagetup <n.kulikouskaya@gmail.com>
2023-12-14 12:15:29 +0100

    feat: add what season task implementation

--- src/test.js
+++ src/test.js
@@ -1,24 +1,42 @@
-function sortByHeight(arr) {
-    const indexStaticCounts = [];
-  
-    const newArr = arr.filter((n, index) => {
-      if (n === -1) {
-        indexStaticCounts.push(index);
-      } else {
-        console.log(n);
-        return n;
+function getSeason(date) {
+    if (date == null) return 'Unable to determine the time of year!'; // check if date is passed
+    console.log(date.hasOwnProperty("toString"));
+    if (date instanceof Date) {
+      if (Object.prototype.toString.call(date) !== '[object Date]' || date.hasOwnProperty("toString")) { //check if date is Date
+        throw new Error('Invalid date!');
+      } 
+    
+      let month = date.getMonth();
+    
+      if (month > 1 && month < 5 ) {
+        return 'spring'
+      } else if (month > 4 && month < 8 ) {
+        return 'summer'
+      } else if (month > 7 && month < 11 ) {
+        return 'autumn'
       }
-    });
-
-    console.log(newArr);
-
-    newArr.sort((a, b) => a - b);
+      else {
+        return 'winter'
+      }
+    }
   
-    indexStaticCounts.forEach(count => {
-      newArr.splice(count, 0, -1);
-    });
-    return newArr;
-  }
+    throw new Error('Invalid date!')
+}
 
-  arr = [-1, 150, 190, 170, -1, -1, 160, 180]
-  sortByHeight(arr);
+makeFake = () => {
+        const fakeDate = {
+            toString() {
+                return Date.prototype.toString.call(new Date());
+            },
+            [Symbol.toStringTag]: 'Date'
+        };
+
+     Object.setPrototypeOf(fakeDate, Object.getPrototypeOf(new Date()));
+     return fakeDate;
+}
+
+let trueData = new Date(1456, 0, 2, 1, 50, 9, 238);
+let fakeData = makeFake();
+getSeason(trueData);
+
+getSeason(fakeData);
--- src/what-season.js
+++ src/what-season.js
@@ -9,11 +9,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * @example
  * 
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (date == null) return 'Unable to determine the time of year!'; // check if date is passed
+  console.log(date.hasOwnProperty(toString));
+  if (date instanceof Date) {
+    if (Object.prototype.toString.call(date) !== '[object Date]' || date.hasOwnProperty("toString")) { //check if date is Date
+      throw new Error('Invalid date!');
+    }
+  
+    let month = date.getMonth();
+  
+    if (month > 1 && month < 5 ) {
+      return 'spring'
+    } else if (month > 4 && month < 8 ) {
+      return 'summer'
+    } else if (month > 7 && month < 11 ) {
+      return 'autumn'
+    }
+    else {
+      return 'winter'
+    }
+  }
+
+  throw new Error('Invalid date!')
 }
 
 module.exports = {
